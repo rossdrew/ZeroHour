8a525f5481b74d809709bc98c83079ca249d03ab
Ross Drew <ross.drew@evogi.com>
2015-11-11 09:23:08 +0000

    Some refactoring of parent script

--- integration_rally-trello.py
+++ integration_rally-trello.py
@@ -22,25 +22,44 @@ TRELLO_TOKEN 	= ""
 options = [arg for arg in sys.argv[1:] if arg.startswith('--')]
 args    = [arg for arg in sys.argv[1:] if arg not in options]
 
-for option in options:
-	parts = option[2:].split('=')
-	optionName = parts[0]
-	optionValue = parts[1]
+def parseCommandLineOptions(cmdLineOptions):
+	"""Parse arguments into script constants"""
+	global RALLY_SERVER, RALLY_USER, RALLY_PASSWORD
+	global TRELLO_USER, TRELLO_SERVER, TRELLO_BOARD, TRELLO_LIST, TRELLO_TOKEN
 
-	if optionName == 'rallyServer':
-		RALLY_SERVER = optionValue
-	elif optionName == 'rallyUser':
-		RALLY_USER = optionValue
-	elif optionName == 'rallyPassword':
-		RALLY_PASSWORD = optionValue
-	elif optionName == 'trelloServer':
-		XTRELLO_SERVER= optionValue
-	elif optionName == 'trelloUsername':
-		TRELLO_USER = optionValue
-	elif optionName == 'trelloBoard':
-		TRELLO_BOARD = optionValuel
-	elif optionName == 'trelloList':
-		TRELLO_LIST = optionValuel
+	for option in cmdLineOptions:
+		parts = option[2:].split('=')
+		optionName = parts[0]
+		optionValue = parts[1]
+		#print optionName, optionValue
+	#Rally
+		if optionName == 'rallyServer': 
+			RALLY_SERVER = optionValue
+		elif optionName == 'rallyUser':
+			RALLY_USER = optionValue
+		elif optionName == 'rallyPassword':
+			RALLY_PASSWORD = optionValue
+	#Trello
+		elif optionName == 'trelloServer':
+			TRELLO_SERVER= optionValue
+		elif optionName == 'trelloUsername':
+			TRELLO_USER = optionValue
+		elif optionName == 'trelloBoard':
+			TRELLO_BOARD = optionValuel
+		elif optionName == 'trelloList':
+			TRELLO_LIST = optionValuel
+		else:
+			print "Unknown command line option '{}'".format(optionName)
 
+def parseCommandLineArguments(cmdLineArguments):
+	for argument in cmdLineArguments:
+		print argument
+
+
+################################
+#        Script Start          #
+################################
+
+parseCommandLineOptions(options)
 
 print "{} connecting to {}".format(RALLY_USER, RALLY_SERVER)
